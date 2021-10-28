109a72cc0435ab880c05b2f31693f8283d66f93f
Anelise Bergin <anelise.bergin@gmail.com>
2021-10-27 19:11:12 -0600

    colors added to homepage status

---
+++ src/components/Job.js
@@ -0,0 +1,50 @@
+import { Link } from "react-router-dom"
+
+const Job = ({job}) => {
+
+  let color;
+  switch (job.fields.status) {
+    case 'Application In Progress':
+      color = '#fee440'
+      break;
+    case 'Application Submitted':
+      color = '#f5b700'
+      break;
+    case 'Interviewing':
+      color = '#04e762'
+      break;
+    case 'Final Interview':
+      color = '#dc0073'
+      break;
+    case 'Offer Accepted':
+      color = '#00f5d4'
+      break;
+    case 'Archived':
+      color = 'white'
+      break;
+  }
+
+  return (
+    <div>
+      <div key={job.id} className="job-list">
+          <Link to={"/job/" + job.id} key={job.id}>
+          <div className="job-text">
+              <div className="left-text">
+                <p>{job.fields.name}</p>
+              </div>   
+            <div
+              className="right-text"
+              style={{
+                backgroundColor: color
+              }}
+            >
+                <p>{job.fields.status}</p>
+              </div>
+          </div>
+          </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Job;
--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -1,63 +1,32 @@
 import './Jobs.css';
-import { Link } from "react-router-dom"
-// import { useState } from "react"
-// import DropdownButton from 'react-bootstrap/DropdownButton';
-// import Dropdown from 'react-bootstrap/Dropdown'
+import Job from './Job.js'
+// import { Link } from "react-router-dom"
 
 
 const JobList = ({ jobs }) => {
 
-  // const [status, setStatus] = useState('');
-
-  // const handleSelect = (e) => {
-  //   console.log(e)
-  //   setStatus(e)
-  // }
-
-  // let color;
-
-  // switch (job.fields.status) {
-  //   case 'Application In Progress':
-  //     color = 'yellow'
-  //     break;
-  //   case 'Application Submitted':
-  //     color = 'pink'
-  //     break;
-  //   case 'Interviewing':
-  //     color = 'red'
-  //     break;
-  //   case 'Final Interview':
-  //     color = 'blue'
-  //     break;
-  //   case 'Offer Accepted':
-  //     color = 'green'
-  //     break;
-  //   case 'Archived':
-  //     color = 'grey'
-  //     break;
-  // }
 
   return (
     <div className="job-list">
       {jobs.map((job) => (
         // insert job component here with job={job} lines 44–60
-        <div key={job.id} className="job-list">
-          <Link to={"/job/" + job.id} key={job.id}>
-          <div className="job-text">
-              <div className="left-text">
-                <p>{job.fields.name}</p>
-              </div>   
-            <div
-              className="right-text"
-              // style={{
-              //   backgroundColor: color
-              // }}
-            >
-                <p>{job.fields.status}</p>
-              </div>
-          </div>
-          </Link>
-        </div>
+        <Job
+          job={job}
+        />
+        // <div key={job.id} className="job-list">
+        //   <Link to={"/job/" + job.id} key={job.id}>
+        //   <div className="job-text">
+        //       <div className="left-text">
+        //         <p>{job.fields.name}</p>
+        //       </div>   
+        //     <div
+        //       className="right-text"
+        //     >
+        //         <p>{job.fields.status}</p>
+        //       </div>
+        //   </div>
+        //   </Link>
+        // </div>
       ))}
 
 
